a69847f34542f8024266853386208367662b770b
Sergey Khmelevskoy <xs290@me.com>
2021-05-11 14:17:25 +0300

    add node-sass, linters, packages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-import App from './App'
-import React from 'react'
-import ReactDOM from 'react-dom'
-ReactDOM.render( <App/>, document.getElementById( 'root' ) )
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import 'core-js/features/array/from';
+import 'core-js/features/number/is-nan';
+import viewportUnitsBuggyfill from 'viewport-units-buggyfill';
+
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './sass/main.sass';
+
+viewportUnitsBuggyfill.init({ force: true, refreshDebounceWait: 250 });
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
