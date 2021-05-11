cb1b05d04b2d478e426397e8263dcbc5177feb45
Ron Dominator <andrei@buzz.fit>
2021-05-11 11:24:20 +0300

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './sass/main.sass'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Homepage from './components/Homepage'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Header/>
+			<Switch>
+				<Route exact path="/" component={Homepage} key="home"/>
+			</Switch>
+			<Footer/>
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/Helper.js
@@ -0,0 +1,3 @@
+export const baseTitle = 'Buzz Fit'
+export const sliderSettingsOne = {dots: false, infinite: true, speed: 300, slidesToShow: 1, slidesToScroll: 1, arrows: true, autoplay: false, accessibility: true}
+export const sliderSettingsThree = {dots: false, infinite: true, speed: 300, slidesToShow: 3, slidesToScroll: 1, arrows: true, autoplay: false, accessibility: true}
